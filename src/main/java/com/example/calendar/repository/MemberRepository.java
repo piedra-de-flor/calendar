9468fcff812c6b190fe4368df67f4df2ebf8be85
piedra-de-flor <xxspdlqjxx@naver.com>
2024-11-17 01:29:32 +0900

    1차 스프린트 완료

--- src/main/java/com/example/calendar/repository/MemberRepository.java
+++ src/main/java/com/example/calendar/repository/MemberRepository.java
@@ -4,7 +4,6 @@ import com.example.calendar.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
