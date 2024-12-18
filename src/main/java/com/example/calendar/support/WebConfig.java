3d36b31bae6983d75355d96d403f20f8d390196b
piedra-de-flor <xxspdlqjxx@naver.com>
2024-12-19 00:30:16 +0900

    feat : WebConfig 추가

---
+++ src/main/java/com/example/calendar/support/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.calendar.support;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://woodking2") // React 서버의 IP
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 쿠키 허용
+                .maxAge(3600); // 캐싱 시간 설정
+    }
+}
